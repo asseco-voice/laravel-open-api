b7d4040fa7aeb54f6398c0ddf68f2f71331595bd
Josip Milotić <42002911+josip-milotic@users.noreply.github.com>
2021-07-28 12:41:18 +0200

    Operation ID changes #patch (#10)
    * Changed mapping
    
    * Changed operationId tag, updated README
    
    * Apply fixes from StyleCI
    
    * Update README.md
    
    * Update OperationIdHandler.php
    
    Co-authored-by: jmilotic <josip.milotic@asseco-see.hr>
    Co-authored-by: Marko Dupor <Norgul@users.noreply.github.com>

--- src/Handlers/OperationIdHandler.php
+++ src/Handlers/OperationIdHandler.php
@@ -3,6 +3,7 @@
 namespace Asseco\OpenApi\Handlers;
 
 use Asseco\OpenApi\Exceptions\OpenApiException;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class OperationIdHandler extends AbstractHandler
@@ -12,10 +13,10 @@ class OperationIdHandler extends AbstractHandler
      * @param string $candidate
      * @param string $method
      * @param string $operation
-     * @return mixed|string
+     * @return string
      * @throws OpenApiException
      */
-    public static function handle(array $tags, string $candidate, string $method, string $operation)
+    public static function handle(array $tags, string $candidate, string $method, string $operation): string
     {
         if (!$tags) {
             return self::generateOperationId($method, $operation, $candidate);
@@ -26,7 +27,7 @@ class OperationIdHandler extends AbstractHandler
 
         self::verifyParameters(count($split));
 
-        return $split[0];
+        return self::generateOperationId($method, $operation, Arr::get($split, '0', $candidate));
     }
 
     /**
@@ -48,21 +49,19 @@ class OperationIdHandler extends AbstractHandler
      */
     protected static function generateOperationId(string $method, string $operation, string $candidate): string
     {
-        switch ($operation) {
-            case 'get':
-                if ($method == 'index') {
-                    return 'getAll' . Str::plural($candidate);
-                }
-
-                return 'get' . $candidate . 'ByPrimaryKey';
-            case 'post':
-                return 'post' . $candidate;
-            case 'put':
-            case 'patch':
-            case 'destroy':
-                return $operation . $candidate . 'ByPrimaryKey';
-            default:
+        switch ($method) {
+            case 'index':
+                return 'getAll' . Str::plural($candidate);
+            case 'store':
+                return 'create' . $candidate;
+            case 'show':
+                return 'get' . $candidate;
+            case 'update':
                 return $operation . $candidate;
+            case 'destroy':
+                return 'delete' . $candidate;
+            default:
+                return $method . $candidate;
         }
     }
 }
